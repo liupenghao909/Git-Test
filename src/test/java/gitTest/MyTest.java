a6d36d9f9f5191a18d70b522bd3ced267e62c657
ant <liupenghao909@163.com>
2022-04-30 22:28:00 +0800

    third commit

--- src/test/java/gitTest/MyTest.java
+++ src/test/java/gitTest/MyTest.java
@@ -3,5 +3,7 @@ package gitTest;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("Hello,Git2");
+        System.out.println("Hello,Git3");
     }
 }
